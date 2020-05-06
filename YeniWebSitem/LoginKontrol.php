8fb6dc7990caa88c0e5a5e66421ea6797d967bd6
Emirhan-Cevik <63146687+Emirhan-Cevik@users.noreply.github.com>
2020-05-06 14:57:51 +0300

    besinciAdim

--- YeniWebSitem/LoginKontrol.php
+++ YeniWebSitem/LoginKontrol.php
@@ -7,15 +7,6 @@ if (isset($_POST['Gonder'])) {
 		echo "<script>alert('HOŞGELDİNİZ b191210039'); location.href='index.html'</script>";		
 	}
 
-	function valid_email( $str )
-	{
-	return ( ! preg_match ( "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str ) ) ? FALSE : TRUE;
-	}
-
-	if (!valid_email($_POST['KullanıcıAd'])) {
-		echo "<script>alert('GİRİLEN DEĞER E-POSTA OLMALIDIR'); location.href='login.html'</script>";
-	}
-
 	else if($_POST['KullanıcıAd']=="" || $_POST['Sifre']==""){
 		echo "<script>alert('KULLANICI ADI VEYA ŞİFRE BOŞ BIRAKILAMAZ'); location.href='login.html'</script>";
 	}
@@ -23,7 +14,6 @@ if (isset($_POST['Gonder'])) {
 	else{
 		echo "<script>alert('KULLANICI ADI VEYA ŞİFRE HATALI'); location.href='login.html'</script>";
 	}
-
 
 }
 
